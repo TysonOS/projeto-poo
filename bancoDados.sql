8cd968d4d04317ba402e6f1fb6067647d0ed9a82
TysonOS <tysonoliveira2013@gmail.com>
2025-03-01 11:04:18 -0300

    update script de criação do banco

--- bancoDados.sql
+++ bancoDados.sql
@@ -1,22 +1,21 @@
-drop database if exists study_tracker
+drop database if exists study_tracker;
 create database study_tracker;
 use study_tracker;
-
---Um assunto funciona como uma lista que contém as tarefas/tópicos de estudo
+-- Um assunto funciona como uma lista que contém as tarefas/tópicos de estudo
 create table assunto(
     id INT auto_increment PRIMARY KEY,
     titulo VARCHAR(60),
     descricao VARCHAR(255),
-    dataCriacao timestamp default current_timestamp
+    dataCriacao DATE
 );
 
 create table tarefas(
     id INT auto_increment PRIMARY KEY,
     assuntoId INT,    
-    foreign key (assuntoId) references assunto(id),
+    foreign key (assuntoId) references assunto(id) ON DELETE CASCADE,
     titulo VARCHAR(60),
     descricao VARCHAR(255),
     concluida BOOLEAN default false,
     dataCriacao TIMESTAMP default current_timestamp,
-    prazoConclusao DATETIME default null
+    prazoConclusao DATE default null
 );
